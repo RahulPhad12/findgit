1ba6f9e5d524ca3db5dac1dce81eabfd4d03fedf
Rahul Phad <101962301+RahulPhad12@users.noreply.github.com>
2022-10-12 18:30:40 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className="card footer">
+        <h2>FindGit | Developed by Rahul | &copy; 2022</h2>
+      </div>
+    )
+  }
+  
+  export default Footer;
